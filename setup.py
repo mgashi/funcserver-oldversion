a8e8383aa28a77a2eb55808cfed187804bd081e5
mgashi <mgashi@cccom.at>
2016-10-24 18:07:33 +0200

    - Adding the setup

---
+++ setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup, find_packages
+
+long_description = ""
+try:
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst', format='markdown_github')
+except:
+    print """
+    README.md could not be converted to rst format.
+    Make sure pypandoc is installed.
+    """
+
+version = '0.1.1'
+setup(
+    name="funcserver",
+    version=version,
+    description="Simple and opiniated way to build APIs in Python",
+    long_description=long_description,
+    keywords='funcserver',
+    author='Prashanth Ellina',
+    author_email="Use the github issues",
+    url="https://github.com/mgashi/funcserver-oldversion",
+    download_url="https://github.com/mgashi/funcserver-oldversion.git",
+    license='MIT License',
+    install_requires=[
+        'gevent',
+        'statsd',
+        'requests',
+        'tornado',
+        'msgpack-python',
+        'basescript',
+    ],
+    package_dir={'funcserver': 'funcserver'},
+    packages=find_packages('.'),
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 1.1",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+    ]
+)
